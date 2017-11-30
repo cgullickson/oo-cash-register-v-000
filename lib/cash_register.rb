355b73d56c8b225c311b7c2df0c09a2c344324ef
Chris Gullickson <cgullickson5@gmail.com>
2017-11-30 21:08:41 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -22,7 +22,7 @@ attr_accessor :total, :discount, :title, :price, :quantity
 
  def apply_discount
      @total = @total*0.8
-     puts "After the discount, the total comes to #{@total}."
+     return "After the discount, the total comes to #{@total}."
 
  end
 
