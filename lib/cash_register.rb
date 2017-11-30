00e8e991f07706e792f1d9a4e636b6d969eef0d0
Chris Gullickson <cgullickson5@gmail.com>
2017-11-30 20:40:31 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,8 +4,12 @@ attr_accessor :total, :employee_discount
 
  def initialize (total = 0, employee_discount)
    @total = total
+   total = 0
    @employee_discount = employee_discount
  end
 
-
+ def total 
+   @total
+ end
+ 
 end
