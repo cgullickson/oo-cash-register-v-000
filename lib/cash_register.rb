ae2da127936cc8cfcb4534efc2ee75375ac9124c
Chris Gullickson <cgullickson5@gmail.com>
2017-11-30 21:05:50 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -20,8 +20,17 @@ attr_accessor :total, :discount, :title, :price, :quantity
    @@items << title
  end
 
+ def apply_discount
+   @total = @total*.8
+
+ end
+
  def items
    @@items
+ end
+
+ def void_last_transaction
+
  end
  
 end
